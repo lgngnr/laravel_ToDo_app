352f4b91d5a901051962e59e47a6b0857e802ae4
Luigi Nigrelli <luiginigrelli@me.com>
2021-03-16 21:03:47 +0100

    remove unusd import

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-
 class DashboardController extends Controller
 {
     public function __construct()
